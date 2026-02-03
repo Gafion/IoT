e14c039db49650c4fb30b1cb912cc6aba5519a26
Christian Egede Colberg <kroll94@gmail.com>
2026-02-03 11:00:30 +0100

    Restructure to monorepo

--- deploy/pi-setup.sh
+++ deploy/pi-setup.sh
@@ -8,4 +8,15 @@ sudo apt install -y python3 python3-venv python3-serial git
 
 cd ~
 if [ ! -d "IoT" ]; then
-  git clone https://github.com/gafion
+  git clone https://github.com/Gafion/IoT.git
+fi
+
+cd  ~/IoT/gateway
+python3 -m venv venv
+./venv/bin/pip install -r requirements.txt
+
+sudo cp gateway.service /etc/systemd/system/iot-gateway.service
+sudo systemctl daemon-reload
+sudo systemctl enable iot-gateway
+
+echo "=== Setup complete ==="
